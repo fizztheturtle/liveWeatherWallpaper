fffffa0ad6b744f2cc711742140a3f7d7abeccde
faisalk16 <15063379@stu.mmu.ac.uk>
2018-06-04 23:20:23 +0100

    fixed all bugs

--- app/src/main/java/comfizztheturtle/httpsgithub/liveweatherwallpaper/MainActivity.java
+++ app/src/main/java/comfizztheturtle/httpsgithub/liveweatherwallpaper/MainActivity.java
@@ -1,7 +1,13 @@
 package comfizztheturtle.httpsgithub.liveweatherwallpaper;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button showGalleryBtn = (Button) findViewById(R.id.btn_show_gallery);
+        showGalleryBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent galleryIntent = new Intent(MainActivity.this,
+                        SpaceGalleryActivity.class);
+                startActivity(galleryIntent);
+            }
+        });
+
+        Button showGif = (Button) findViewById(R.id.btn_show_gif);
+        showGif.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gifIntent = new Intent(MainActivity.this,
+                        gifsActivity.class);
+                startActivity(gifIntent);
+            }
+        });
+
     }
 }
+//    ImageView imageView = (ImageView) findViewById(R.id.imageView);
+
+//Loading image from below url into imageView
+//        Glide.with(this)
+//                .load("http://goo.gl/gEgYUd")
+//                .override(200, 200)
+//                .centerCrop()
+//                .into(imageView);
+
+
+
+
+
---
+++ app/src/main/java/comfizztheturtle/httpsgithub/liveweatherwallpaper/gifsActivity.java
@@ -0,0 +1,27 @@
+package comfizztheturtle.httpsgithub.liveweatherwallpaper;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+
+public class gifsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gifs);
+
+        ImageView gifImageView = (ImageView) findViewById(R.id.iv_gif);
+
+        Glide.with(this)
+                .load("http://i.imgur.com/Vth6CBz.gif")
+                .asBitmap()
+                .placeholder(R.drawable.ic_cloud_off_red)
+                .error(R.drawable.ic_cloud_off_red)
+                .into(gifImageView);
+    }
+
+}
